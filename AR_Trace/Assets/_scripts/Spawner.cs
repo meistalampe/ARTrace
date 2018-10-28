76cb1cec10f8561a92bfb6d0acb93eca43ddf53f
meistalampe <32868456+meistalampe@users.noreply.github.com>
2018-10-28 13:54:58 +0100

    initial push

---
+++ AR_Trace/Assets/_scripts/Spawner.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spawner : MonoBehaviour {
+
+	public Transform spawnPos;
+	public GameObject spawnee;
+	
+	// Update is called once per frame
+	void Update () {
+		if(Input.GetKeyDown(KeyCode.Space))
+        {
+			Instantiate(spawnee, spawnPos.position, spawnPos.rotation);
+            Debug.Log("Space key was pressed.");
+        }
+        if (Input.GetKeyUp(KeyCode.Space))
+        {
+            Debug.Log("Space key was released.");
+        }
+    }
+}
